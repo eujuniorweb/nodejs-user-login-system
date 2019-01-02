686c64541bd6e3f3d832c1deebc1086c209ab831
Junior Silva <jros2.silva@gmail.com>
2019-01-02 01:16:02 -0300

    Setup inicial, configuração de middlewares necessários para o sistema de login.

---
+++ app.js
@@ -0,0 +1,75 @@
+var createError = require('http-errors');
+var express = require('express');
+var validator = require('express-validator');
+var messages = require('express-messages');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var multer = require('multer');
+var upload = multer({dest:'./uploads'});
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Handle Sessions
+app.use(session({
+    secret:'secret',
+    saveUninitialized:true,
+    resave:true
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Validator
+
+// Flash Messages
+app.use(flash());
+app.use(function (req, res, next) {
+    res.locals.messages = messages(req, res);
+    next();
+});
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
