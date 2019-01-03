bb45841c730dec1d110e6a94d1d4e3c56d6baed3
Junior Silva <jros2.silva@gmail.com>
2019-01-03 01:44:49 -0300

    Implementação da Views de registro e login

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
 
 var app = express();
 
@@ -54,7 +54,7 @@ app.use(function (req, res, next) {
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
